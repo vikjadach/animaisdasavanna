8604c11324d647dad8fe4d97f4650b18f7350283
vitoria <victoriaferjc@gmail.com>
2024-03-26 11:56:29 -0300

    animaissavanna

---
+++ animaisdasavana/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace animaisdasavana;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ animaisdasavana/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace animaisdasavana;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
